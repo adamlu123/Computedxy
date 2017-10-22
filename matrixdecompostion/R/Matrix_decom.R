a6983305075f77aa21a174b6418280fea155acfb
Yadong Lu <yadongl1@ics.uci.edu>
2017-10-22 11:19:12 -0700

    add solving function

--- matrixdecompostion/R/Matrix_decom.R
+++ matrixdecompostion/R/Matrix_decom.R
@@ -11,24 +11,66 @@ GramSchm <- function(A){
   v = A[,1]
   R[1,1] <- sqrt(sum(v^2))
   Q[,1] <- v/R[1,1]
-  
+
   for(j in 2:ncol(A)){
     v <- A[,j]
     for(i in 1:(j-1)){
       R[i,j] <- t(Q[,i]) %*% A[,j]
       v <- v - R[i,j] * Q[,i]
     }
-    #print(v)
     R[j,j] <- sqrt(sum(v^2))
     Q[,j] <- v/R[j,j]
   }
-  print(Q%*%R)
-  print(R)
-  return(Q)  
+  # stopifnot((Q%*%R - A))
+  return(list(Q=Q,R=R))
 }
-GramSchm(ma)
+rslt <- GramSchm(dt)
 
 
 Householder <- function(A){
-  
+
 }
+
+
+
+solve <- function(X,y){
+  #'Input X, response y
+  #'Output LSE
+  n <- ncol(X)
+  beta <- rep( NA, n)
+  rslt <- GramSchm(X)
+  Q <- rslt[[1]]
+  R <- rslt[[2]]
+  rhs <- t(Q) %*% y
+  beta[n] <- rhs[n] / R[n,n]
+  for(i in (n-1):1){
+    v <- rhs[i]
+    for(j in n:(i+1)){
+      v <- v- R[i,j] * beta[j]
+    }
+    beta[i] <- v / R[i,i]
+    #print(beta)
+  }
+  return(beta)
+}
+
+beta <- solve(dt[,1:5], dt[,6] )
+
+
+fit <- lm(y ~ 0+ X1 + X2 + X3 + X4 + X5, data=dt)
+summary(fit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
